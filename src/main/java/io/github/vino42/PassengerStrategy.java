12b5370a8f9f5860c8ffa09b60c85b0e8cb5ba89
vino42 <38912428@qq.com>
2023-10-06 15:44:06 +0800

    优化

--- src/main/java/io/github/vino42/PassengerStrategy.java
+++ src/main/java/io/github/vino42/PassengerStrategy.java
@@ -24,16 +24,23 @@ public class PassengerStrategy implements Strategy {
         List<User> passengers = bus.getPassengers();
         List<User> list = waitingPassenger.stream().filter(d -> d.getDirection().equals(bus.getDirection())).toList();
         //优先保障故障车乘客
-        List<User> toUpPassenger = list.stream().sorted(Comparator.comparingInt(o -> o.getPriority().get())).toList();
-
+        List<User> toUpPassenger = list.stream().filter(d -> d.getPriority().get() > 0).sorted(Comparator.comparing(o -> o.getPriority().get())).toList();
+        //当前车上乘客没满
+        List<User> users = new ArrayList<>();
         if (passengers.size() < 29) {
-            List<User> users;
+            //算出来剩余的空位数
             int shengyu = 29 - passengers.size();
+            //如果故障车乘客有的话
             if (!toUpPassenger.isEmpty()) {
-
+                //剩余空位数大于故障车数优先故障乘客上车
                 if (shengyu > toUpPassenger.size()) {
                     users = toUpPassenger;
+                    //故障乘客上车后,还有空余位置。填充下正常乘客
+                    List<User> normalUser = list.stream().filter(d -> !toUpPassenger.contains(d)).toList();
+                    List<User> normalUserToLoad = normalUser.subList(0, shengyu - toUpPassenger.size());
+                    users.addAll(normalUserToLoad);
                 } else {
+                    //剩余空位装不下故障乘客,那就把能装的全装上
                     users = toUpPassenger.subList(0, shengyu + 1);
                 }
             } else {
@@ -48,11 +55,9 @@ public class PassengerStrategy implements Strategy {
             bus.setPassengers(passengers);
             //站台减人
             currentStation.getWaitingPassenger().removeAll(users);
-            TimeUnit.SECONDS.sleep(users.size() * 10L);
-            System.out.println("线程号："+Thread.currentThread().getId()+"车号：" + bus.getBusNumber() + (bus.getDirection() == 1 ? "正向" : "反向") + "乘客上车| 当前站" + currentStation.getIndex() + "上车乘客数" + users.size());
-
-
         }
+        TimeUnit.SECONDS.sleep(users.size() * 10L);
+        System.out.println("线程号：" + Thread.currentThread().getId() + " " + "车号：" + bus.getBusNumber() + (bus.getDirection() == 1 ? "正向" : "反向") + "乘客上车| 当前站" + currentStation.getIndex() + "上车乘客数" + users.size());
 
 
     }
@@ -65,18 +70,15 @@ public class PassengerStrategy implements Strategy {
         passengers.removeAll(offPassenger);
         bus.setPassengers(passengers);
         //正常来说 该故障车需要停顿
-//        TimeUnit.SECONDS.sleep(offPassenger.size() * 10L);
-        System.out.println("线程号："+Thread.currentThread().getId()+"车号：" + bus.getBusNumber() + (bus.getDirection() == 1 ? "正向" : "反向") + "乘客下车| 当前站" + currentStation.getIndex() + "下车乘客数" + offPassenger.size());
+        TimeUnit.SECONDS.sleep(10L);
+        System.out.println("线程号：" + Thread.currentThread().getId() + " " + "车号：" + bus.getBusNumber() + (bus.getDirection() == 1 ? "正向" : "反向") + "乘客下车| 当前站" + currentStation.getIndex() + "下车乘客数" + offPassenger.size());
     }
 
     @Override
     public void busBroken(Station currentStation, Bus bus) {
         List<User> passengers = bus.getPassengers();
         //设置当前乘客上车地点为当前站
-        List<User> list = passengers.stream().peek(d -> d.addPriority()).map(u -> {
-            u.setUpStation(currentStation);
-            return u;
-        }).toList();
+        List<User> list = passengers.stream().peek(User::addPriority).peek(u -> u.setUpStation(currentStation)).toList();
         //当前站的乘客排队列别添加下车乘客
         currentStation.getWaitingPassenger().addAll(list);
         //全员下车
